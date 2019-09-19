899f502d63d3af43e34f34c540bb5350c4ca76aa
DESKTOP-DMNMGC7\Lucas <lucasdanielcopes@gmail.com>
2019-09-18 21:50:52 -0300

    Cambio de tamaño en los textbox para visivilizar mejor decimales

--- Practica1/Practica1/Matrices.Designer.cs
+++ Practica1/Practica1/Matrices.Designer.cs
@@ -67,217 +67,217 @@
             // 
             // m11
             // 
-            this.m11.Location = new System.Drawing.Point(49, 86);
+            this.m11.Location = new System.Drawing.Point(40, 87);
             this.m11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m11.Name = "m11";
-            this.m11.Size = new System.Drawing.Size(30, 20);
+            this.m11.Size = new System.Drawing.Size(91, 20);
             this.m11.TabIndex = 0;
             // 
             // m12
             // 
-            this.m12.Location = new System.Drawing.Point(79, 86);
+            this.m12.Location = new System.Drawing.Point(135, 87);
             this.m12.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m12.Name = "m12";
-            this.m12.Size = new System.Drawing.Size(30, 20);
+            this.m12.Size = new System.Drawing.Size(91, 20);
             this.m12.TabIndex = 1;
             // 
             // m13
             // 
-            this.m13.Location = new System.Drawing.Point(110, 86);
+            this.m13.Location = new System.Drawing.Point(230, 87);
             this.m13.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m13.Name = "m13";
-            this.m13.Size = new System.Drawing.Size(30, 20);
+            this.m13.Size = new System.Drawing.Size(91, 20);
             this.m13.TabIndex = 2;
             // 
             // m15
             // 
-            this.m15.Location = new System.Drawing.Point(171, 86);
+            this.m15.Location = new System.Drawing.Point(420, 87);
             this.m15.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m15.Name = "m15";
-            this.m15.Size = new System.Drawing.Size(30, 20);
+            this.m15.Size = new System.Drawing.Size(91, 20);
             this.m15.TabIndex = 4;
             this.m15.Visible = false;
             // 
             // m14
             // 
-            this.m14.Location = new System.Drawing.Point(141, 86);
+            this.m14.Location = new System.Drawing.Point(325, 87);
             this.m14.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m14.Name = "m14";
-            this.m14.Size = new System.Drawing.Size(30, 20);
+            this.m14.Size = new System.Drawing.Size(91, 20);
             this.m14.TabIndex = 3;
             this.m14.Visible = false;
             // 
             // m25
             // 
-            this.m25.Location = new System.Drawing.Point(171, 106);
+            this.m25.Location = new System.Drawing.Point(420, 107);
             this.m25.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m25.Name = "m25";
-            this.m25.Size = new System.Drawing.Size(30, 20);
+            this.m25.Size = new System.Drawing.Size(91, 20);
             this.m25.TabIndex = 9;
             this.m25.Visible = false;
             // 
             // m24
             // 
-            this.m24.Location = new System.Drawing.Point(141, 106);
+            this.m24.Location = new System.Drawing.Point(325, 107);
             this.m24.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m24.Name = "m24";
-            this.m24.Size = new System.Drawing.Size(30, 20);
+            this.m24.Size = new System.Drawing.Size(91, 20);
             this.m24.TabIndex = 8;
             this.m24.Visible = false;
             // 
             // m23
             // 
-            this.m23.Location = new System.Drawing.Point(110, 106);
+            this.m23.Location = new System.Drawing.Point(230, 107);
             this.m23.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m23.Name = "m23";
-            this.m23.Size = new System.Drawing.Size(30, 20);
+            this.m23.Size = new System.Drawing.Size(91, 20);
             this.m23.TabIndex = 7;
             // 
             // m22
             // 
-            this.m22.Location = new System.Drawing.Point(79, 106);
+            this.m22.Location = new System.Drawing.Point(135, 107);
             this.m22.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m22.Name = "m22";
-            this.m22.Size = new System.Drawing.Size(30, 20);
+            this.m22.Size = new System.Drawing.Size(91, 20);
             this.m22.TabIndex = 6;
             // 
             // m21
             // 
-            this.m21.Location = new System.Drawing.Point(49, 106);
+            this.m21.Location = new System.Drawing.Point(40, 107);
             this.m21.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m21.Name = "m21";
-            this.m21.Size = new System.Drawing.Size(30, 20);
+            this.m21.Size = new System.Drawing.Size(91, 20);
             this.m21.TabIndex = 5;
             // 
             // m35
             // 
-            this.m35.Location = new System.Drawing.Point(171, 125);
+            this.m35.Location = new System.Drawing.Point(420, 126);
             this.m35.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m35.Name = "m35";
-            this.m35.Size = new System.Drawing.Size(30, 20);
+            this.m35.Size = new System.Drawing.Size(91, 20);
             this.m35.TabIndex = 14;
             this.m35.Visible = false;
             // 
             // m34
             // 
-            this.m34.Location = new System.Drawing.Point(141, 125);
+            this.m34.Location = new System.Drawing.Point(325, 126);
             this.m34.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m34.Name = "m34";
-            this.m34.Size = new System.Drawing.Size(30, 20);
+            this.m34.Size = new System.Drawing.Size(91, 20);
             this.m34.TabIndex = 13;
             this.m34.Visible = false;
             // 
             // m33
             // 
-            this.m33.Location = new System.Drawing.Point(110, 125);
+            this.m33.Location = new System.Drawing.Point(230, 126);
             this.m33.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m33.Name = "m33";
-            this.m33.Size = new System.Drawing.Size(30, 20);
+            this.m33.Size = new System.Drawing.Size(91, 20);
             this.m33.TabIndex = 12;
             // 
             // m32
             // 
-            this.m32.Location = new System.Drawing.Point(79, 125);
+            this.m32.Location = new System.Drawing.Point(135, 126);
             this.m32.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m32.Name = "m32";
-            this.m32.Size = new System.Drawing.Size(30, 20);
+            this.m32.Size = new System.Drawing.Size(91, 20);
             this.m32.TabIndex = 11;
             // 
             // m31
             // 
-            this.m31.Location = new System.Drawing.Point(49, 125);
+            this.m31.Location = new System.Drawing.Point(40, 126);
             this.m31.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m31.Name = "m31";
-            this.m31.Size = new System.Drawing.Size(30, 20);
+            this.m31.Size = new System.Drawing.Size(91, 20);
             this.m31.TabIndex = 10;
             // 
             // m45
             // 
-            this.m45.Location = new System.Drawing.Point(171, 144);
+            this.m45.Location = new System.Drawing.Point(420, 145);
             this.m45.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m45.Name = "m45";
-            this.m45.Size = new System.Drawing.Size(30, 20);
+            this.m45.Size = new System.Drawing.Size(91, 20);
             this.m45.TabIndex = 19;
             this.m45.Visible = false;
             // 
             // m44
             // 
-            this.m44.Location = new System.Drawing.Point(141, 144);
+            this.m44.Location = new System.Drawing.Point(325, 145);
             this.m44.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m44.Name = "m44";
-            this.m44.Size = new System.Drawing.Size(30, 20);
+            this.m44.Size = new System.Drawing.Size(91, 20);
             this.m44.TabIndex = 18;
             this.m44.Visible = false;
             // 
             // m43
             // 
-            this.m43.Location = new System.Drawing.Point(110, 144);
+            this.m43.Location = new System.Drawing.Point(230, 145);
             this.m43.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m43.Name = "m43";
-            this.m43.Size = new System.Drawing.Size(30, 20);
+            this.m43.Size = new System.Drawing.Size(91, 20);
             this.m43.TabIndex = 17;
             this.m43.Visible = false;
             // 
             // m42
             // 
-            this.m42.Location = new System.Drawing.Point(79, 144);
+            this.m42.Location = new System.Drawing.Point(135, 145);
             this.m42.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m42.Name = "m42";
-            this.m42.Size = new System.Drawing.Size(30, 20);
+            this.m42.Size = new System.Drawing.Size(91, 20);
             this.m42.TabIndex = 16;
             this.m42.Visible = false;
             // 
             // m41
             // 
-            this.m41.Location = new System.Drawing.Point(49, 144);
+            this.m41.Location = new System.Drawing.Point(40, 145);
             this.m41.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m41.Name = "m41";
-            this.m41.Size = new System.Drawing.Size(30, 20);
+            this.m41.Size = new System.Drawing.Size(91, 20);
             this.m41.TabIndex = 15;
             this.m41.Visible = false;
             // 
             // m55
             // 
-            this.m55.Location = new System.Drawing.Point(171, 163);
+            this.m55.Location = new System.Drawing.Point(420, 164);
             this.m55.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m55.Name = "m55";
-            this.m55.Size = new System.Drawing.Size(30, 20);
+            this.m55.Size = new System.Drawing.Size(91, 20);
             this.m55.TabIndex = 24;
             this.m55.Visible = false;
             // 
             // m54
             // 
-            this.m54.Location = new System.Drawing.Point(141, 163);
+            this.m54.Location = new System.Drawing.Point(325, 164);
             this.m54.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m54.Name = "m54";
-            this.m54.Size = new System.Drawing.Size(30, 20);
+            this.m54.Size = new System.Drawing.Size(91, 20);
             this.m54.TabIndex = 23;
             this.m54.Visible = false;
             // 
             // m53
             // 
-            this.m53.Location = new System.Drawing.Point(110, 163);
+            this.m53.Location = new System.Drawing.Point(230, 164);
             this.m53.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m53.Name = "m53";
-            this.m53.Size = new System.Drawing.Size(30, 20);
+            this.m53.Size = new System.Drawing.Size(91, 20);
             this.m53.TabIndex = 22;
             this.m53.Visible = false;
             // 
             // m52
             // 
-            this.m52.Location = new System.Drawing.Point(79, 163);
+            this.m52.Location = new System.Drawing.Point(135, 164);
             this.m52.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m52.Name = "m52";
-            this.m52.Size = new System.Drawing.Size(30, 20);
+            this.m52.Size = new System.Drawing.Size(91, 20);
             this.m52.TabIndex = 21;
             this.m52.Visible = false;
             // 
             // m51
             // 
-            this.m51.Location = new System.Drawing.Point(49, 163);
+            this.m51.Location = new System.Drawing.Point(40, 164);
             this.m51.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.m51.Name = "m51";
-            this.m51.Size = new System.Drawing.Size(30, 20);
+            this.m51.Size = new System.Drawing.Size(91, 20);
             this.m51.TabIndex = 20;
             this.m51.Visible = false;
             // 
@@ -285,7 +285,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(10, 22);
+            this.label16.Location = new System.Drawing.Point(171, 9);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(282, 24);
             this.label16.TabIndex = 45;
@@ -293,7 +293,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 275);
+            this.button1.Location = new System.Drawing.Point(530, 283);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 26);
             this.button1.TabIndex = 39;
@@ -311,7 +311,7 @@
             "3x3",
             "4x4",
             "5x5"});
-            this.checkedListBox1.Location = new System.Drawing.Point(49, 198);
+            this.checkedListBox1.Location = new System.Drawing.Point(41, 264);
             this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(58, 45);
@@ -324,7 +324,7 @@
             this.label15.BackColor = System.Drawing.Color.LightGreen;
             this.label15.Location = new System.Drawing.Point(29, 65);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(248, 255);
+            this.label15.Size = new System.Drawing.Size(612, 255);
             this.label15.TabIndex = 47;
             // 
             // comboBox1
@@ -333,7 +333,7 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Gauss-Jordan",
             "Gauss-Seidel"});
-            this.comboBox1.Location = new System.Drawing.Point(110, 198);
+            this.comboBox1.Location = new System.Drawing.Point(103, 264);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(90, 21);
@@ -341,45 +341,45 @@
             // 
             // r5
             // 
-            this.r5.Location = new System.Drawing.Point(201, 163);
+            this.r5.Location = new System.Drawing.Point(530, 164);
             this.r5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.r5.Name = "r5";
-            this.r5.Size = new System.Drawing.Size(54, 20);
+            this.r5.Size = new System.Drawing.Size(88, 20);
             this.r5.TabIndex = 54;
             this.r5.Visible = false;
             // 
             // r4
             // 
-            this.r4.Location = new System.Drawing.Point(201, 144);
+            this.r4.Location = new System.Drawing.Point(530, 145);
             this.r4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.r4.Name = "r4";
-            this.r4.Size = new System.Drawing.Size(54, 20);
+            this.r4.Size = new System.Drawing.Size(88, 20);
             this.r4.TabIndex = 53;
             this.r4.Visible = false;
             // 
             // r3
             // 
-            this.r3.Location = new System.Drawing.Point(201, 125);
+            this.r3.Location = new System.Drawing.Point(530, 126);
             this.r3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.r3.Name = "r3";
-            this.r3.Size = new System.Drawing.Size(54, 20);
+            this.r3.Size = new System.Drawing.Size(88, 20);
             this.r3.TabIndex = 52;
             this.r3.TextChanged += new System.EventHandler(this.R3_TextChanged);
             // 
             // r2
             // 
-            this.r2.Location = new System.Drawing.Point(201, 106);
+            this.r2.Location = new System.Drawing.Point(530, 107);
             this.r2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.r2.Name = "r2";
-            this.r2.Size = new System.Drawing.Size(54, 20);
+            this.r2.Size = new System.Drawing.Size(88, 20);
             this.r2.TabIndex = 51;
             // 
             // r1
             // 
-            this.r1.Location = new System.Drawing.Point(201, 86);
+            this.r1.Location = new System.Drawing.Point(530, 87);
             this.r1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.r1.Name = "r1";
-            this.r1.Size = new System.Drawing.Size(54, 20);
+            this.r1.Size = new System.Drawing.Size(88, 20);
             this.r1.TabIndex = 50;
             // 
             // Matrices
@@ -387,7 +387,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.NavajoWhite;
-            this.ClientSize = new System.Drawing.Size(319, 343);
+            this.ClientSize = new System.Drawing.Size(666, 343);
             this.Controls.Add(this.r5);
             this.Controls.Add(this.r4);
             this.Controls.Add(this.r3);
