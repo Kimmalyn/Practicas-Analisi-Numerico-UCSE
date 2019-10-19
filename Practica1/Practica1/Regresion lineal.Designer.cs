01f6d98314308f03650cd82a29da0135b325285e
LUCAS\Lucas <lucasdanielcopes@gmail.com>
2019-10-19 10:32:55 -0300

    finalizacion regresion polinomial (faltan pruebas)

--- Practica1/Practica1/Regresion lineal.Designer.cs
+++ Practica1/Practica1/Regresion lineal.Designer.cs
@@ -36,65 +36,74 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label16
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(27, 27);
-            this.label16.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label16.Location = new System.Drawing.Point(14, 14);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(669, 44);
+            this.label16.Size = new System.Drawing.Size(354, 24);
             this.label16.TabIndex = 46;
-            this.label16.Text = "GAUSS-JORDAN Y GAUSS-SEIDEL";
+            this.label16.Text = "REGRESION LINEAL Y POLINOMIAL";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(112, 146);
+            this.textBox1.Location = new System.Drawing.Point(56, 76);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 31);
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
             this.textBox1.TabIndex = 47;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(112, 187);
+            this.textBox2.Location = new System.Drawing.Point(56, 97);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 31);
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
             this.textBox2.TabIndex = 48;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(74, 105);
+            this.label1.Location = new System.Drawing.Point(37, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 49;
             this.label1.Text = "Nuevo par";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(75, 149);
+            this.label2.Location = new System.Drawing.Point(38, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 25);
+            this.label2.Size = new System.Drawing.Size(17, 13);
             this.label2.TabIndex = 50;
             this.label2.Text = "X:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(74, 190);
+            this.label3.Location = new System.Drawing.Point(37, 99);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 25);
+            this.label3.Size = new System.Drawing.Size(17, 13);
             this.label3.TabIndex = 51;
             this.label3.Text = "Y:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 235);
+            this.button1.Location = new System.Drawing.Point(41, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 55);
+            this.button1.Size = new System.Drawing.Size(92, 29);
             this.button1.TabIndex = 52;
             this.button1.Text = "Añadir";
             this.button1.UseVisualStyleBackColor = true;
@@ -102,20 +111,52 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(80, 314);
+            this.button2.Location = new System.Drawing.Point(40, 198);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(181, 55);
+            this.button2.Size = new System.Drawing.Size(90, 29);
             this.button2.TabIndex = 53;
             this.button2.Text = "Calcular";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            " RL Minimos Cuadrados",
+            " RP Minimos Cuadrados"});
+            this.comboBox1.Location = new System.Drawing.Point(41, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 21);
+            this.comboBox1.TabIndex = 54;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 55;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "Grado:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.NavajoWhite;
-            this.ClientSize = new System.Drawing.Size(1338, 633);
+            this.ClientSize = new System.Drawing.Size(440, 248);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -124,6 +165,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label16);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -141,5 +183,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
     }
 }
---
+++ Practica1/Practica1/Regresion lineal.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<double> puntosx = new List<double>();
+        List<double> puntosy = new List<double>();
+        double exy = 0;
+        int n = 0;
+        //Ex
+        double ex = 0;
+        //Ey
+        double ey = 0;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            puntosx.Add(double.Parse(textBox1.Text));
+            puntosy.Add(double.Parse(textBox2.Text));
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //N
+            n = puntosx.Count;
+
+            //SUMATORIA X.Y
+            int c = 0;
+            foreach (double item in puntosx)
+            {
+                exy = item * puntosy[c];
+                c++;
+            }
+
+            //Ex
+            ex = puntosx.Sum();
+
+            //Ey
+            ey = puntosy.Sum();
+
+            //Ex^2
+            double ex2 = 0;
+            foreach (double item in puntosx)
+            {
+                ex2 = ex2 + Math.Pow(item, 2);
+            }
+
+            //(Ex)^2
+            double ex2b = 0;
+            ex2b = Math.Pow(ex, 2);
+
+            //double a1 = (puntosx.Count * ());
+
+           if (comboBox1.SelectedIndex == 0)
+           {
+            
+           }
+           else
+           {
+                int grado = int.Parse(textBox3.Text);
+                double[,] matrix;
+                matrix = new double[grado-1,grado];
+                ex = 0;
+                ey = 0;
+                
+                for (int i = 0; i < n-1; i++)
+                {
+                    ex += puntosx[i];
+                    ey += puntosy[i];
+                    for (int j = 0; j < grado; j++)
+                    {
+                        for (int k = 0; k < grado; k++)
+                        {
+                            matrix[j, k] += Math.Pow(puntosx[i], j + k);
+                        }
+                        matrix[j, grado + 1] += puntosy[i] * Math.Pow(puntosx[i], j);
+                    }
+                }
+
+                List<double> Resultados = new List<double>();
+                double coeficiente = 0;
+                int a = 0;
+                int b = 0;
+                int d = 0;
+                for (a = 0; a < grado+1; a++)
+                {
+                    coeficiente = matrix[a, a];
+                    for (b = 0; b <= grado+1; b++)
+                    {
+                        matrix[a, b] = matrix[a, b] / coeficiente;
+                    }
+                    for (b = 0; b < grado+1; b++)
+                    {
+                        if (a != b)
+                        {4
+                            coeficiente = matrix[b, a];
+                            for (d = 0; d <= grado+1; d++)
+                            {
+                                matrix[b, d] = matrix[b, d] - (coeficiente * matrix[a, d]);
+                            }
+                        }
+                    }
+                }
+                for (a = 0; a < grado+1; a++)
+                {
+                    Resultados.Add(matrix[a, grado+1]);
+                }
+           }
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
